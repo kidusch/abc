02167b170055aa3291900839c6ad023da4cd7047
kidusch <kidus.dejen@gmail.com>
2022-11-11 09:50:20 +0100

    test8

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -141,7 +141,7 @@ class UserController extends AbstractController
         $email = $request_data["email"];
         //dd($email);
         $check = $this->userRepository->fetchUser($email);
-        $id = $check->getId();
+        $id = $check[['id']];
         if ($check){
             $email = (new Email())
             ->from('info@abc-barber.ch')
