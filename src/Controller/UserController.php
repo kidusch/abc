5e04888c0ac99e38fc0d55eab6d11787035d1d03
kidusch <kidus.dejen@gmail.com>
2022-11-11 09:35:27 +0100

    user exist test2

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -141,6 +141,7 @@ class UserController extends AbstractController
         $email = $request_data["email"];
         //dd($email);
         $check = $this->userRepository->fetchUser($email);
+        $id = $check['id'];
         if ($check){
             $email = (new Email())
             ->from('info@abc-barber.ch')
@@ -151,7 +152,7 @@ class UserController extends AbstractController
             //->priority(Email::PRIORITY_HIGH)
             ->subject('ABC Barber - Réinitialiserr mot de passe')
             ->text('ABC Barber - Réinitialiser mot de passe')
-            ->html("<h1>Réinitialiser ton mot de passe</h1></br></br><p>Clique sur le lien pour réinitialiser ton mot de passe: https://api.abc-barber.ch/forget/".$email);
+            ->html("<h1>Réinitialiser ton mot de passe</h1></br><p>Clique sur le lien pour réinitialiser ton mot de passe: https://api.abc-barber.ch/forget/".$email);
 
             $mailer->send($email);
         } else {
